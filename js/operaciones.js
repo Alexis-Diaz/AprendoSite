c842103a123ed36a4748eb4bd2528b9b453c0cf2
Alexis-Diaz <alex.d4556@gmail.com>
2022-07-14 12:05:23 -0600

    Se ha trabajado para que la pagina consejos sea receptiva a diferentes tamaños de pantallas

---
+++ js/operaciones.js
@@ -0,0 +1,41 @@
+
+const regularExpression = /index.html|pages\/register.html|pages\/categories.html|pages\/consejos.html/
+const UrlGlobal = location.pathname.replace(regularExpression, "");
+let isProduction = false;
+
+function redirectToRegister (){
+    // console.log(location.protocol);
+    // console.log(location.hostname);
+    // console.log(location.port);
+    // console.log(location.pathname);
+
+    if(isProduction){
+        window.location.href = "/pages/register.html"
+    }else{
+        window.location.href = `${UrlGlobal}pages/register.html`
+    }
+}
+
+function redirectToCategories (){
+    if(isProduction){
+         window.location.href = "/pages/categories.html"
+    }else{
+        window.location.href = `${UrlGlobal}pages/categories.html`
+    }
+}
+
+function redirectToConsejos (){
+    if(isProduction){
+        window.location.href = "/pages/consejos.html"
+    }else{
+        window.location.href = `${UrlGlobal}pages/consejos.html`
+    }
+}
+
+function redirectToIndex (){
+    if(isProduction){
+        window.location.href = "/"
+    }else{
+        window.location.href = `${UrlGlobal}index.html`
+    }
+}
